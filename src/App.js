b2a81dabbc8b462372bfde932af61c790fa7bf63
Yash Mishra <praisedboy5000@gmail.com>
2023-11-22 16:47:56 +0530

    Added some media query for quiz page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import ForgetPassword from './components/ForgotPassword'
+import VerifyOTP from './components/VerifyOTP'
 import Quiz from './components/Quiz'
-
 
 function App() {
   return (
