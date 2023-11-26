37a69a198c7b71d6f09eeb834b5f2034a3507047
Yash Mishra <praisedboy5000@gmail.com>
2023-11-26 12:50:42 +0530

    design changesin quiz part

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 import Human from "./images/MainPageMobile.svg";
 import bars from "./images/threebars.svg";
 import whitebars from "./images/whitebars.svg";
-import Arrow from "./images/Circlearrow.svg";
 
 function Quiz() {
   const [ishidden, setIshidden] = useState("container");
@@ -15,10 +14,7 @@ function Quiz() {
   const [isbody, setisbody] = useState("hack");
   const [istexty, setistexty] = useState("white");
   const [iscontainer, setiscontainer] = useState("section1");
-  const [isContainer, setisContainer] = useState("section1");
-  const [isCOntainer, setisCOntainer] = useState("section1");
-  const [isCONtainer, setisCONtainer] = useState("section1");
-  const [iscontaineR, setiscontaineR] = useState('nocircles');
+
   const HandleClickFunction = () => {
     setIshidden("hidden");
     setiswhitebox("nobox");
@@ -29,10 +25,6 @@ function Quiz() {
     setisbody("nobody");
     setistexty("nobody");
     setiscontainer("section1");
-    setisContainer("section1");
-    setisCOntainer("section1");
-    setisCONtainer("section1 ");
-    setiscontaineR('nocircles')
   };
   const HandleClickAgain = () => {
     setIshidden("container");
@@ -43,32 +35,6 @@ function Quiz() {
     setiscss("hide");
     setisbody("hack");
     setistexty("white");
-  };
-  const Handle = () => {
-    setiscontainer("sectionq");
-    setisContainer("section1");
-    setisCOntainer("section1");
-    setisCONtainer("section1");
-  };
-  const Handle1 = () => {
-    setisContainer("sectionq");
-    setiscontainer("section1");
-    setisCOntainer("section1");
-    setisCONtainer("section1");
-  };
-  const Handle2 = () => {
-    setisCOntainer("sectionq");
-    setiscontaineR('')
-    setiscontainer("section1");
-    setisContainer("section1");
-    setisCONtainer("section1");
-  };
-  const Handle3 = () => {
-    setisCONtainer("sectionq");
-    setiscontaineR('')
-    setiscontainer("section1");
-    setisCOntainer("section1");
-    setisContainer("section1");
   };
   return (
     <div className={ishidden}>
@@ -93,82 +59,15 @@ function Quiz() {
           HACKA<div className={istexty}>ZONE</div>
         </div>
         <div className={iscontainer}>
-          Section 1
-          <div className="arrow">
-            <img src={Arrow} alt="ImageNA" className="Arrow" onClick={Handle} />
-            <div>
-              <div className="c1">1</div>
-              <div className="c2">2</div>
-              <div className="c3">3</div>
-              <div className="c4">4</div>
-              <div className="c5">5</div>
-              <div className="c6">6</div>
-              <div className="c7">7</div>
-              <div className="c8">8</div>
-            </div>
-          </div>
-        </div>
-        <div className={isContainer}>
-          Section 2
-          <div className="arrow">
-            <img
-              src={Arrow}
-              alt="ImageNA"
-              className="Arrow"
-              onClick={Handle1}
-            />
-            <div>
-              <div className="c1">1</div>
-              <div className="c2">2</div>
-              <div className="c3">3</div>
-              <div className="c4">4</div>
-              <div className="c5">5</div>
-              <div className="c6">6</div>
-              <div className="c7">7</div>
-              <div className="c8">8</div>
-            </div>
-          </div>
-        </div>
-        <div className={isCOntainer}>
-          Section 3
-          <div className="arrow">
-            <img
-              src={Arrow}
-              alt="ImageNA"
-              className="Arrow"
-              onClick={Handle2}
-            />
-            <div className={iscontaineR}>
-              <div className="c1">1</div>
-              <div className="c2">2</div>
-              <div className="c3">3</div>
-              <div className="c4">4</div>
-              <div className="c5">5</div>
-              <div className="c6">6</div>
-              <div className="c7">7</div>
-              <div className="c8">8</div>
-            </div>
-          </div>
-        </div>
-        <div className={isCONtainer}>
-          Section 4
-          <div className="arrow">
-            <img
-              src={Arrow}
-              alt="ImageNA"
-              className="Arrow"
-              onClick={Handle3}
-            />
-            <div className={iscontaineR}>
-              <div className="c1">1</div>
-              <div className="c2">2</div>
-              <div className="c3">3</div>
-              <div className="c4">4</div>
-              <div className="c5">5</div>
-              <div className="c6">6</div>
-              <div className="c7">7</div>
-              <div className="c8">8</div>
-            </div>
+          <div>
+            <div className="c1">1</div>
+            <div className="c2">2</div>
+            <div className="c3">3</div>
+            <div className="c4">4</div>
+            <div className="c5">5</div>
+            <div className="c6">6</div>
+            <div className="c7">7</div>
+            <div className="c8">8</div>
           </div>
         </div>
       </section>
